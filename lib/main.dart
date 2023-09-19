92db0aa142f2ef1a2d4bf941bc66a49219c26adc
SyLV <sy2000dn0@gmail.com>
2023-09-19 07:36:37 +0700

    Add student to ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_ui/student.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp(
-
-        home: DynamicUI(),
-        debugShowCheckedModeBanner: false,
-      )
+    return const SafeArea(
+        child: MaterialApp(
+          home: DynamicUI(),
+          debugShowCheckedModeBanner: false,
+        )
     );
   }
 }
@@ -27,7 +27,10 @@ class DynamicUI extends StatefulWidget {
 }
 
 class _DynamicUIState extends State<DynamicUI> {
-  List<String> items = ["Item 1", "2"];
+  List<Student> students = [];
+  TextEditingController nameControl = TextEditingController();
+  TextEditingController idControl = TextEditingController();
+  TextEditingController addressControl = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,22 +40,72 @@ class _DynamicUIState extends State<DynamicUI> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
+          SizedBox(
             height: 300,
             child: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(items[index]),
-                );
-              },
+              itemCount: students.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.all(2.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('ID: ${students[index].id}'),
+                        Text('Tên: ${students[index].name}'),
+                        Text('Tuổi: ${students[index].address}'),
+                      ],
+                    ),
+                  );
+                }
             ),
-          )
-          ,
-          ElevatedButton(onPressed: (){}, child: const Text("Add")),
+          ),
+          TextField(
+            decoration: const InputDecoration(
+                labelText: 'Name',
+                hintText: 'Le Van Sy',
+                border: UnderlineInputBorder()
+            ),
+            controller: nameControl,
+          ),
+
+          TextField(
+            decoration: const InputDecoration(
+                labelText: 'ID',
+                hintText: '123456',
+                border: UnderlineInputBorder()
+            ),
+            controller: idControl,
+          ),
+
+          TextField(
+            decoration: const InputDecoration(
+                labelText: 'Address',
+                hintText: 'HaDong',
+                border: UnderlineInputBorder()
+            ),
+            controller: addressControl,
+          ),
+          ElevatedButton(onPressed: addStudent, child: const Text("Add")),
         ],
       ),
     );
+  }
+
+  void addStudent(){
+    String name = nameControl.text;
+    String id = idControl.text;
+    String address = addressControl.text;
+    if (name.isNotEmpty && int.tryParse(id)!= null && address.isNotEmpty){
+      setState(() {
+        students.add(Student(name, address, int.tryParse(id)!));
+        nameControl.text = "";
+        idControl.text = "";
+        addressControl.text = "";
+      });
+    }
   }
 }
 
