80e9155633a4f6bb6a3cfaa1a1066fc0b396270c
HP <sy2000dn0@gmail.com>
2023-10-04 10:53:17 +0700

    split file main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:sample_firestore/views/create_account.dart';
+import 'package:sample_firestore/views/sample_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,64 +17,10 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Read Firestore Data'),
         ),
-        body: FirestoreExample(),
+        body: SampleFirestore(),
       ),
     );
   }
 }
 
-class FirestoreExample extends StatelessWidget {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: SizedBox(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: _firestore.collection('users').snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return CircularProgressIndicator(); // Hiển thị tiến trình nếu đang tải dữ liệu.
-                }
-                final documents = snapshot.data!.docs;
-                return ListView.builder(
-                  itemCount: documents.length,
-                  itemBuilder: (context, index) {
-                    final document = documents[index];
-                    final data = document.data() as Map<String, dynamic>;
-                    final username = data['username'];
-                    final avatar = data['avatar'];
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Image.network(
-                          avatar,
-                          height: 60,
-                          width: 100,
-                        ),
-                        Text(username,
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            MaterialPageRoute route = MaterialPageRoute(builder: (context) => CreateAccount());
-            Navigator.push(context, route);
-          },
-          child: Text('Add user'),
-        ),
-      ],
-    );
-  }
-}
